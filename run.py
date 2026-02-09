84e6b4ddc197076114ae4260f57cdf83e983b604
Adam Kabli <kabliadam17@gmail.com>
2026-02-09 11:43:00 +0100

    railway ready

--- run.py
+++ run.py
@@ -52,4 +52,6 @@ if __name__ == "__main__":
         print("Tables created successfully!\n")
     
     print("🚀 Starting DocHub application...")
-    app.run(debug=True)
+    ########################app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
